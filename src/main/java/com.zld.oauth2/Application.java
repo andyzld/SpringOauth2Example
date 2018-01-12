adb76ee5fce7bb0a4f64ffa3151039a0008f1674
zld <1329733245@qq.com>
2018-01-12 09:52:07 +0800

    springOauth20180112

---
+++ src/main/java/com.zld.oauth2/Application.java
@@ -0,0 +1,17 @@
+package com.zld.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Administrator on 2018/1/11.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) throws Throwable {
+        int i = 1;
+        System.out.println("ss1q");
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/main/java/com.zld.oauth2/conf/TestEndpoints.java
@@ -0,0 +1,29 @@
+package com.zld.oauth2.conf;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Administrator on 2018/1/12.
+ */
+@RestController
+public class TestEndpoints {
+//暴露两个接口 一个开启认证 一个不开启 保护应用，那必须得先有一些资源
+    @GetMapping("/product/{id}")
+    public String getProduct(@PathVariable String id) {
+        //for debug
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return "product id : " + id;
+    }
+
+    @GetMapping("/order/{id}")
+    public String getOrder(@PathVariable String id) {
+        //for debug
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return "order id : " + id;
+    }
+
+}
